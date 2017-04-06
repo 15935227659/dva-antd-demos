1d9c902466861437557af810eb6f936e9707f8d1
walkerqiao <150643694@qq.com>
2017-04-06 16:46:13 +0800

    update

--- lumen_api/app/Models/Report.php
+++ lumen_api/app/Models/Report.php
@@ -16,6 +16,9 @@ final class Report extends Model
 
         $end = $request->input('end') ?? date('Y-m-d', strtotime('-1 day'));
         $start = $request->input('start') ?? date('Y-m-d', strtotime('-15 day'));
+
+        $days = ceil((strtotime($end) - strtotime($start)) % 86400);
+
         $terminal = $request->input('dim_type') ?? 4; // 默认微信
         $orderStatus = $request->input('gmv_type') ?? 1; // 下单
         $bizzType = $request->input('dim_det') ?? 1; // 搜索业务
@@ -28,6 +31,45 @@ final class Report extends Model
                   ->whereBetween('dt', [$start, $end])
                   ->orderBy('dt', 'DESC')
                   ->get();
+
+        $newRes = [];
+        foreach($result as $row) {
+            $newRes[$row->dt] = $row;
+        }
+        $result = array();
+
+        for($et = strtotime($end), $st = strtotime($start); $et >= $st; $et = $et - 86400) {
+            $theDay = date('Y-m-d', $et);
+            if(!isset($newRes[$theDay])) {
+                $row = new \stdClass();
+                $row->dt = $theDay;
+                $result[] = $row;
+            } else {
+                $result[] = $newRes[$theDay];
+            }
+        }
         return $result;
     }
 }
+
+/**
+        if(sizeof($result) !== $days) { // 日期范围缺失数据补齐
+            $new = [];
+            foreach($result as $row) {
+                $new[$row['dt']] = $row;
+            }
+
+            $result = [];
+            for($et = strtotime($end), $st = strtotime($start); $et <= $st; $et = $et - 86400) {
+                if(!isset($new[date('Y-m-d', $et)]) {
+                    $result[] = [
+                        'dt' => date('Y-m-d', $et),
+                    ];
+                } else {
+                    $result[] = $new[date('Y-m-d', $et)];
+                }
+            }
+        }
+
+        print_r($result);exit();
+*/
